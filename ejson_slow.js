2b0c8d1654d0fb398947741fd899b43c59394cad
Lucas Hansen <lucashansen@gmail.com>
2015-11-10 11:41:47 -0800

    Make test document much deeper

--- ejson_slow.js
+++ ejson_slow.js
@@ -10,7 +10,9 @@ if (Meteor.isClient) {
         orderID = Orders.findOne()._id,
         activeQueries = 30,
         queries = _.range(activeQueries).map(i => {
-          return Tracker.autorun(comp => Orders.findOne(orderID, {1: 1}));
+          return Tracker.autorun(comp => Orders.findOne(orderID, {
+            fields: {1: 1}
+          }));
         });
 
       // Perform database update
@@ -30,12 +32,29 @@ if (Meteor.isClient) {
   });
 }
 
+
 if (Meteor.isServer) {
+
+  // Randomly generate a deep tree
+  let makeTree = function(width, depth, decay) {
+    if (depth === 0)
+      return Math.random();
+
+    let tree = {};
+    _.range(width).forEach((i) => {
+      let newWidth = (width-1) * (decay < Math.random()),
+          newDepth = (depth-1) * (decay < Math.random());
+      tree[i] = makeTree(newWidth, newDepth,  decay);
+    });
+
+    return tree;
+  };
 
   // Insert large document
   if (!Orders.findOne()) {
     let order = {};
-    _.range(8000).forEach(i => order[i] = i);
+    while (EJSON.stringify(order).length < 1000000)
+      order = makeTree(25, 7, 0.6);
     Orders.insert(order);
   }
 
